ce92b1ff7fd8220fa5cd393b7593321a4a0aa04d
Ryan Magoon <mr.magoon5@gmail.com>
2016-06-30 11:46:31 -0400

    added RedditArticle Component

--- app.ts
+++ app.ts
@@ -2,6 +2,63 @@ import { bootstrap } from "@angular/platform-browser-dynamic";
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'reddit-article',
+  host: {
+    class: 'row'
+  },
+  template: `
+    <div class="four wide column center aligned votes">
+      <div class="ui statistic">
+        <div class="value">
+          {{ votes }}
+        </div>
+        <div class="label">
+          Points
+        </div>
+      </div>
+    </div>
+    <div class="twelve wide column">
+      <a class="ui large header" href="{{ link }}">
+        {{ title }}
+      </a>
+      <ul class="ui big horizontal list voters">
+        <li class="item">
+          <a href (click)="voteUp()">
+            <i class="arrow up icon"></i>
+              upvote
+          </a>
+        </li>
+        <li class="item">
+          <a href (click)="voteDown()">
+            <i class="arrow down icon"></i>
+            downvote
+          </a>
+        </li>
+      </ul>
+    </div>
+  `
+})
+class ArticleComponent {
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp() {
+    this.votes += 1;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+  }
+}
+
+@Component({
     selector: 'reddit',
     template: `
       <form class="ui large form segment">
