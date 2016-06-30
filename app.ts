4ade70bb6394f77d8fef8f931f97f490a487dc15
Ryan Magoon <mr.magoon5@gmail.com>
2016-06-30 12:03:11 -0400

    refactor voting logic into article class

--- app.ts
+++ app.ts
@@ -11,6 +11,14 @@ class Article {
     this.link = link;
     this.votes = votes || 0;
  }
+
+ voteUp(): void {
+   this.votes += 1;
+ }
+
+ voteDown(): void {
+   this.votes -= 1;
+ }
 }
 
 @Component({
@@ -58,12 +66,12 @@ class ArticleComponent {
   }
 
   voteUp(): boolean {
-    this.votes += 1;
+    this.article.voteUp();
     return false;
   }
 
   voteDown(): boolean {
-    this.votes -= 1;
+    this.article.voteDown();
     return false;
   }
 }
