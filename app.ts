55b10cf09a67f59397092af41c0ff18c1970fe21
Ryan Magoon <mr.magoon5@gmail.com>
2016-06-29 22:10:30 -0400

    Add name to hello world template

--- app.ts
+++ app.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'hello-world',
-    template: `
-    <div>
-      Hello world
-    </div>
-    `
+    template: `<div>Hello {{ name }}</div>`
 })
 class HelloWorld {
+  name: string;
 
+  constructor() {
+    this.name = 'Ryan';
+  }
 }
 
 bootstrap(HelloWorld);
