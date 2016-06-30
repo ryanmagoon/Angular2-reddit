d311e9ee198045f1f4fef23eea1d7db4cf9c33b6
Ryan Magoon <mr.magoon5@gmail.com>
2016-06-29 21:57:16 -0400

    Add hello-world component to index.html

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-// your code goes here
+import { bootstrap } from "@angular/platform-browser-dynamic";
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'hello-world',
+    template: `
+    <div>
+      Hello world
+    </div>
+    `
+})
+class HelloWorld {
+
+}
+
+bootstrap(HelloWorld);
