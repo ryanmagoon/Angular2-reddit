6c5c476145412dbc72e58553563e4995b221f8b7
Ryan Magoon <mr.magoon5@gmail.com>
2016-06-30 11:03:11 -0400

    remove hello-world component and add reddit component

--- app.ts
+++ app.ts
@@ -2,19 +2,26 @@ import { bootstrap } from "@angular/platform-browser-dynamic";
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'hello-world',
+    selector: 'reddit',
     template: `
-    <ul>
-      <li *ngFor="let name of names">Hello {{ name }}</li>
-    </ul>
+      <form class="ui large form segment>
+        <h3 class="ui header">Add a Link</h3>
+
+        <div class="field">
+          <label for="title">Title:</label>
+          <input name="title">
+        </div>
+
+        <div class="field">
+          <label for="link">Link:</label>
+          <input name="link">
+        </div>
+      </form>
     `
 })
-class HelloWorld {
-  names: string[];
-
+class RedditApp {
   constructor() {
-    this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
   }
 }
 
-bootstrap(HelloWorld);
+bootstrap(RedditApp);
